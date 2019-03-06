0b665f0f5bc6f16b4e4fcb4d959a96301389d84e
Cody Cook <codycook96@gmail.com>
2019-03-06 02:49:20 -0600

    Add files via upload

---
+++ Release/moc_printmanager.cpp
@@ -0,0 +1,199 @@
+/****************************************************************************
+** Meta object code from reading C++ file 'printmanager.h'
+**
+** Created by: The Qt Meta Object Compiler version 67 (Qt 5.9.1)
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include "../../Source/printmanager.h"
+#include <QtCore/qbytearray.h>
+#include <QtCore/qmetatype.h>
+#if !defined(Q_MOC_OUTPUT_REVISION)
+#error "The header file 'printmanager.h' doesn't include <QObject>."
+#elif Q_MOC_OUTPUT_REVISION != 67
+#error "This file was generated using the moc from 5.9.1. It"
+#error "cannot be used with the include files from this version of Qt."
+#error "(The moc has changed too much.)"
+#endif
+
+QT_BEGIN_MOC_NAMESPACE
+QT_WARNING_PUSH
+QT_WARNING_DISABLE_DEPRECATED
+struct qt_meta_stringdata_PrintManager_t {
+    QByteArrayData data[24];
+    char stringdata0[480];
+};
+#define QT_MOC_LITERAL(idx, ofs, len) \
+    Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
+    qptrdiff(offsetof(qt_meta_stringdata_PrintManager_t, stringdata0) + ofs \
+        - idx * sizeof(QByteArrayData)) \
+    )
+static const qt_meta_stringdata_PrintManager_t qt_meta_stringdata_PrintManager = {
+    {
+QT_MOC_LITERAL(0, 0, 12), // "PrintManager"
+QT_MOC_LITERAL(1, 13, 25), // "on_lineEdit_returnPressed"
+QT_MOC_LITERAL(2, 39, 0), // ""
+QT_MOC_LITERAL(3, 40, 32), // "on_tableWidget_cellDoubleClicked"
+QT_MOC_LITERAL(4, 73, 3), // "row"
+QT_MOC_LITERAL(5, 77, 6), // "column"
+QT_MOC_LITERAL(6, 84, 6), // "launch"
+QT_MOC_LITERAL(7, 91, 11), // "std::string"
+QT_MOC_LITERAL(8, 103, 4), // "file"
+QT_MOC_LITERAL(9, 108, 12), // "openSelected"
+QT_MOC_LITERAL(10, 121, 10), // "cacheTable"
+QT_MOC_LITERAL(11, 132, 11), // "updateTable"
+QT_MOC_LITERAL(12, 144, 10), // "updateItem"
+QT_MOC_LITERAL(13, 155, 28), // "on_pushButton_Search_clicked"
+QT_MOC_LITERAL(14, 184, 27), // "on_pushButton_Reset_clicked"
+QT_MOC_LITERAL(15, 212, 26), // "on_pushButton_Open_clicked"
+QT_MOC_LITERAL(16, 239, 26), // "on_pushButton_Edit_clicked"
+QT_MOC_LITERAL(17, 266, 28), // "on_pushButton_Delete_clicked"
+QT_MOC_LITERAL(18, 295, 32), // "on_menuChangeDirectory_triggered"
+QT_MOC_LITERAL(19, 328, 30), // "on_actionImportNames_triggered"
+QT_MOC_LITERAL(20, 359, 28), // "on_menuUpdateNames_triggered"
+QT_MOC_LITERAL(21, 388, 34), // "on_actionExport_to_Excel_trig..."
+QT_MOC_LITERAL(22, 423, 32), // "on_actionCreate_Backup_triggered"
+QT_MOC_LITERAL(23, 456, 23) // "on_actionHelp_triggered"
+
+    },
+    "PrintManager\0on_lineEdit_returnPressed\0"
+    "\0on_tableWidget_cellDoubleClicked\0row\0"
+    "column\0launch\0std::string\0file\0"
+    "openSelected\0cacheTable\0updateTable\0"
+    "updateItem\0on_pushButton_Search_clicked\0"
+    "on_pushButton_Reset_clicked\0"
+    "on_pushButton_Open_clicked\0"
+    "on_pushButton_Edit_clicked\0"
+    "on_pushButton_Delete_clicked\0"
+    "on_menuChangeDirectory_triggered\0"
+    "on_actionImportNames_triggered\0"
+    "on_menuUpdateNames_triggered\0"
+    "on_actionExport_to_Excel_triggered\0"
+    "on_actionCreate_Backup_triggered\0"
+    "on_actionHelp_triggered"
+};
+#undef QT_MOC_LITERAL
+
+static const uint qt_meta_data_PrintManager[] = {
+
+ // content:
+       7,       // revision
+       0,       // classname
+       0,    0, // classinfo
+      18,   14, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       0,       // signalCount
+
+ // slots: name, argc, parameters, tag, flags
+       1,    0,  104,    2, 0x08 /* Private */,
+       3,    2,  105,    2, 0x08 /* Private */,
+       6,    1,  110,    2, 0x08 /* Private */,
+       9,    0,  113,    2, 0x08 /* Private */,
+      10,    0,  114,    2, 0x08 /* Private */,
+      11,    0,  115,    2, 0x08 /* Private */,
+      12,    2,  116,    2, 0x08 /* Private */,
+      13,    0,  121,    2, 0x08 /* Private */,
+      14,    0,  122,    2, 0x08 /* Private */,
+      15,    0,  123,    2, 0x08 /* Private */,
+      16,    0,  124,    2, 0x08 /* Private */,
+      17,    0,  125,    2, 0x08 /* Private */,
+      18,    0,  126,    2, 0x08 /* Private */,
+      19,    0,  127,    2, 0x08 /* Private */,
+      20,    0,  128,    2, 0x08 /* Private */,
+      21,    0,  129,    2, 0x08 /* Private */,
+      22,    0,  130,    2, 0x08 /* Private */,
+      23,    0,  131,    2, 0x08 /* Private */,
+
+ // slots: parameters
+    QMetaType::Void,
+    QMetaType::Void, QMetaType::Int, QMetaType::Int,    4,    5,
+    QMetaType::Void, 0x80000000 | 7,    8,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void, QMetaType::Int, QMetaType::Int,    4,    5,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+
+       0        // eod
+};
+
+void PrintManager::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        PrintManager *_t = static_cast<PrintManager *>(_o);
+        Q_UNUSED(_t)
+        switch (_id) {
+        case 0: _t->on_lineEdit_returnPressed(); break;
+        case 1: _t->on_tableWidget_cellDoubleClicked((*reinterpret_cast< int(*)>(_a[1])),(*reinterpret_cast< int(*)>(_a[2]))); break;
+        case 2: _t->launch((*reinterpret_cast< std::string(*)>(_a[1]))); break;
+        case 3: _t->openSelected(); break;
+        case 4: _t->cacheTable(); break;
+        case 5: _t->updateTable(); break;
+        case 6: _t->updateItem((*reinterpret_cast< int(*)>(_a[1])),(*reinterpret_cast< int(*)>(_a[2]))); break;
+        case 7: _t->on_pushButton_Search_clicked(); break;
+        case 8: _t->on_pushButton_Reset_clicked(); break;
+        case 9: _t->on_pushButton_Open_clicked(); break;
+        case 10: _t->on_pushButton_Edit_clicked(); break;
+        case 11: _t->on_pushButton_Delete_clicked(); break;
+        case 12: _t->on_menuChangeDirectory_triggered(); break;
+        case 13: _t->on_actionImportNames_triggered(); break;
+        case 14: _t->on_menuUpdateNames_triggered(); break;
+        case 15: _t->on_actionExport_to_Excel_triggered(); break;
+        case 16: _t->on_actionCreate_Backup_triggered(); break;
+        case 17: _t->on_actionHelp_triggered(); break;
+        default: ;
+        }
+    }
+}
+
+const QMetaObject PrintManager::staticMetaObject = {
+    { &QMainWindow::staticMetaObject, qt_meta_stringdata_PrintManager.data,
+      qt_meta_data_PrintManager,  qt_static_metacall, nullptr, nullptr}
+};
+
+
+const QMetaObject *PrintManager::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *PrintManager::qt_metacast(const char *_clname)
+{
+    if (!_clname) return nullptr;
+    if (!strcmp(_clname, qt_meta_stringdata_PrintManager.stringdata0))
+        return static_cast<void*>(const_cast< PrintManager*>(this));
+    return QMainWindow::qt_metacast(_clname);
+}
+
+int PrintManager::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QMainWindow::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 18)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 18;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 18)
+            *reinterpret_cast<int*>(_a[0]) = -1;
+        _id -= 18;
+    }
+    return _id;
+}
+QT_WARNING_POP
+QT_END_MOC_NAMESPACE
